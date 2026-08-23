144f77dec9af96dab6b4e7248bf55fc7b22b1fd0
Tom1ko <notix1337@gmail.com>
2026-08-23 20:18:49 +0300

    bug fixes and test was created

--- nn.py
+++ nn.py
@@ -1,43 +1,56 @@
 import random
 
-import matplotlib.pyplot as plt
-import numpy as np
-import torch
-
 from graph import *
 from value import *
 
 
-class Neuron:
+class Module:
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0
+
+    def parameters(self):
+        return []
+
+
+class Neuron(Module):
     def __init__(self, nin: int) -> None:
         self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
         self.b = Value(random.uniform(-1, 1))
 
     def __call__(self, x) -> Value:
-        act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
-        out = act.tanh()
+        out = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
         return out
 
     def parameters(self):
         return self.w + [self.b]
 
 
-class Layer:
-    def __init__(self, nin, nout) -> None:
+class Layer(Module):
+    def __init__(self, nin, nout, act=None) -> None:
         self.neurons = [Neuron(nin) for _ in range(nout)]
+        self.act = act
 
     def __call__(self, x):
-        outs = [n(x) for n in self.neurons]
+        if self.act:
+            outs = [self.act(n(x)) for n in self.neurons]
+        else:
+            outs = [n(x) for n in self.neurons]
+
         return outs[0] if len(outs) == 1 else outs
 
     def parameters(self):
         return [p for neuron in self.neurons for p in neuron.parameters()]
 
 
-class MLP:
-    def __init__(self, nin, nouts) -> None:
+class MLP(Module):
+    def __init__(self, nin, nouts, acts=None) -> None:
         sz = [nin] + nouts
-        self.layers = [Layer(sz[i], sz[i + 1]) for i in range(len(nouts))]
+
+        if acts is None:
+            acts = [None] * len(nouts)
+
+        self.layers = [Layer(sz[i], sz[i + 1], act=acts[i]) for i in range(len(nouts))]
 
     def __call__(self, x):
         for layer in self.layers:
--- value.py
+++ value.py
@@ -26,15 +26,6 @@ class Value:
         out._backward = backward
         return out
 
-    def __radd__(self, other):
-        return self + other
-
-    def __sub__(self, other):
-        return self + (-other)
-
-    def __neg__(self, other):
-        return self * -1
-
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), "*")
@@ -44,14 +35,7 @@ class Value:
             other.grad += self.data * out.grad
 
         out._backward = backward
-
         return out
-
-    def __rmul__(self, other):
-        return self * other
-
-    def __truediv__(self, other):
-        return self * other**-1
 
     def __pow__(self, other):
         assert isinstance(other, (int, float))
@@ -61,7 +45,15 @@ class Value:
             self.grad += other * self.data ** (other - 1) * out.grad
 
         out._backward = backward
+        return out
 
+    def sqrt(self):
+        out = Value(math.sqrt(self.data), (self,), "sqrt")
+
+        def backward():
+            self.grad += (0.5 / (out.data + 1e-7)) * out.grad
+
+        out._backward = backward
         return out
 
     def exp(self):
@@ -70,6 +62,16 @@ class Value:
 
         def backward():
             self.grad += out.data * out.grad
+
+        out._backward = backward
+        return out
+
+    def log(self):
+        x = self.data
+        out = Value(math.log(x + 1e-7), (self,), "log")
+
+        def backward():
+            self.grad += (1 / (x + 1e-7)) * out.grad
 
         out._backward = backward
         return out
@@ -83,7 +85,65 @@ class Value:
             self.grad += (1 - t**2) * out.grad
 
         out._backward = backward
+        return out
 
+    def sigmoid(self):
+        s = 1 / (1 + math.exp(-self.data))
+        out = Value(s, (self,), "sigmoid")
+
+        def backward():
+            self.grad += s * (1 - s) * out.grad
+
+        out._backward = backward
+        return out
+
+    def ReLU(self):
+        out = Value(max(self.data, 0), (self,), "ReLU")
+
+        def backward():
+            self.grad += (out.data > 0) * out.grad
+
+        out._backward = backward
+        return out
+
+    def LeakyReLU(self, alpha: float = 0.01):
+        x = self.data
+        out = Value(max(alpha * x, x), (self,), "LeakyReLU")
+
+        def backward():
+            self.grad += (alpha if x < 0 else 1) * out.grad
+
+        out._backward = backward
+        return out
+
+    def ELU(self, alpha: float = 0.01):
+        x = self.data
+        e = math.exp(x)
+        out = Value(alpha * (e - 1) if x <= 0 else x, (self,), "ELU")
+
+        def backward():
+            self.grad += (alpha * e if x < 0 else 1) * out.grad
+
+        out._backward = backward
+        return out
+
+    def GELU(self):
+        inner = self + 0.44715 * self**3
+
+        tanh_out = (inner * math.sqrt(2 / math.pi)).tanh()
+        cdf = (tanh_out + 1) * 0.5
+
+        return self * cdf
+
+    def SiLU(self):
+        x = self.data
+        s = 1 / (1 + math.exp(-x))
+        out = Value(x * s, (self,), "SiLU")
+
+        def backward():
+            self.grad += (s + x * s * (1 - s)) * out.grad
+
+        out._backward = backward
         return out
 
     def backward(self):
@@ -102,3 +162,24 @@ class Value:
         self.grad = 1.0
         for node in reversed(topo):
             node._backward()
+
+    def __neg__(self):
+        return self * -1
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rsub__(self, other):
+        return other + (-self)
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        return self * (other**-1)
+
+    def __rtruediv__(self, other):
+        return other * (self**-1)
